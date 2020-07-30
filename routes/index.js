d4b7ccba6083fdfd9d7431d4b17fbe44c6d6b8da
sanbunam <softdavidlee@gmail.com>
2020-07-29 20:48:37 -0700

    node route

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,20 @@ const router = express.Router();
 // Do work here
 router.get("/", (req, res) => {
   // res.send("Hey! It works!");
-  res.render("hello", {
-    name: "david",
-    dog: req.query.dog,
-  });
+  // res.render("hello", {
+  //   name: "david",
+  //   dog: req.query.dog,
+  // });
+  // var david = { name: "david", age: 39, sex: "male" };
+  // res.json(david);
+  // res.render("hello");
+  // res.send(`${req.query.name} ${req.query.age}`);
+  res.json(req.query);
+});
+
+router.get("/reverse/:name/:age", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;
