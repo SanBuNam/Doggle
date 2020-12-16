387b131a67bcc9b665e590984ec7a9388eeeb6ea
sanbunam <softdavidlee@gmail.com>
2020-12-15 22:11:06 -0800

    home page

--- helpers.js
+++ helpers.js
@@ -36,10 +36,10 @@ exports.javaScriptMenu = [
 ];
 
 exports.quotes = [
-  {name: "0", quote: "Life is to enjoy no matter what happens.Life is to enjoy no matter what happens.Life is to enjoy no matter what happens."},
-  {name: "1", quote: "Jesus loves me like no other person.Jesus loves me like no other person.Jesus loves me like no other person."},
-  {name: "2", quote: "Life is to enjoy no matter what happens."},
-  {name: "3", quote: "Jesus loves me like no other person."},
+  {name: "0", quote: "내게 능력 주시는 자 안에서 내가 모든 것을 할 수 있느니라 - 빌립보서 4:13"},
+  {name: "1", quote: "내게 능력 주시는 자 안에서 내가 모든 것을 할 수 있느니라 - 빌립보서 4:13"},
+  {name: "2", quote: "내게 능력 주시는 자 안에서 내가 모든 것을 할 수 있느니라 - 빌립보서 4:13"},
+  {name: "3", quote: "내게 능력 주시는 자 안에서 내가 모든 것을 할 수 있느니라 - 빌립보서 4:13"},
   {name: "4", quote: "Life is to enjoy no matter what happens."},
   {name: "5", quote: "Jesus loves me like no other person."},
   {name: "6", quote: "Life is to enjoy no matter what happens."},
