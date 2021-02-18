f49f0fec4549836a1292626e317fe051e2d739f6
sanbunam <softdavidlee@gmail.com>
2021-02-17 20:43:43 -0800

    getting rid of stuff

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 require("dotenv").config({ path: "variables.env" });
 
-mongoose.connect(process.env.DATABASE).catch(err => console.log(err), { autoIndex: false });
+mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise;
 mongoose.connection.on("error", (err) => {
   console.error(`Stop!🚫  → ${err.message}`);
@@ -14,6 +14,8 @@ require("./models/Review");
 
 const app = require("./app");
 app.set("port", process.env.PORT || 7777);
+
 const server = app.listen(app.get("port"), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
